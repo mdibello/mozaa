15621d91e18361f06f13c6506cbabdc1639d50fd
Matthew DiBello <matthew.quickbeam@gmail.com>
2021-07-18 14:48:03 -0400

    Tiles can be placed, grid re-sizes appropriately

--- script.js
+++ script.js
@@ -34,20 +34,25 @@ while (counter > 0) {
 }
 
 let start_tile = tiles.pop();
-grid[32][32].type = "tile";
-grid[32][32].tile = start_tile.id;
-grid[32][32].color = start_tile.color;
+place_tile(32, 32, start_tile);
 
 let current_tile = tiles.pop();
 
-window.addEventListener('load', function(event) {
+window.addEventListener('load', render);
+
+function render() {
     let divs = "";
     let gtas = "";
     for (i = subgrid_start.y; i <= subgrid_end.y; i++) {
         let gta = '"';
         for (j = subgrid_start.x; j <= subgrid_end.x; j++) {
-            divs += `<div class="${grid[j][i].type} tile-${grid[j][i].id}" style="background-color:${grid[j][i].color}"></div>`;
-            gta += `tile-${grid[j][i].id} `;
+            if (is_valid_square(i, j)) {
+                divs += `<div class="${grid[j][i].type}" id="${grid[j][i].id}" x="${j}" y="${i}" style="background-color:${grid[j][i].color}"></div>`;
+                gta += `${grid[j][i].id} `;
+            } else {
+                divs += `<div class="blank ${grid[j][i].id}" style="background-color:white"></div>`;
+                gta += `${grid[j][i].id} `;
+            }
         }
         gta += '" ';
         gtas += gta;
@@ -55,7 +60,52 @@ window.addEventListener('load', function(event) {
     this.document.getElementById("tiles-div").style.gridTemplateAreas = gtas;
     this.document.getElementById("tiles-div").innerHTML = divs;
     this.document.getElementById("tile-new").style.backgroundColor = current_tile.color;
-});
+
+    let spaces = this.document.getElementsByClassName("space");
+    for (let i = 0; i < spaces.length; i++) {
+        spaces[i].addEventListener('mouseover', function(event) {
+            event.target.style.backgroundColor = "lightgray";
+            event.target.style.cursor = "pointer";
+        });
+        spaces[i].addEventListener('mouseout', function(event) {
+            event.target.style.backgroundColor = "gray";
+            event.target.style.cursor = "default";
+        });
+        spaces[i].addEventListener('click', function(event) {
+            let tile_x = parseInt(this.attributes.x.value);
+            let tile_y = parseInt(this.attributes.y.value);
+            place_tile(tile_y, tile_x, current_tile);
+            recalculate_subgrid(tile_x, tile_y);
+            current_tile = tiles.pop();
+            render();
+        });
+    }
+}
+
+function is_valid_square(x, y) {
+    return grid[y-1][x].type == "tile" ||
+           grid[y+1][x].type == "tile" ||
+           grid[y][x-1].type == "tile" ||
+           grid[y][x+1].type == "tile" ||
+           grid[y][x].type == "tile";
+}
+
+function place_tile(x, y, tile) {
+    grid[y][x].type = "tile";
+    grid[y][x].tile = tile.id;
+    grid[y][x].color = tile.color;
+}
+
+function recalculate_subgrid(x, y) {
+    if (x-1 < subgrid_start.x || y-1 < subgrid_start.y) {
+        subgrid_start.x -= 1;
+        subgrid_start.y -= 1;
+    }
+    if (x+1 > subgrid_end.x || y+1 > subgrid_end.y) {
+        subgrid_end.x += 1;
+        subgrid_end.y += 1;
+    }
+}
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
