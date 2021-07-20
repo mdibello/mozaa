484516f2bbcb788119a2e5838ba873e5ba56555b
Matthew DiBello <matthew.quickbeam@gmail.com>
2021-07-19 22:23:51 -0400

    Scoring is mostly implemented, albeit broken

--- script.js
+++ script.js
@@ -117,6 +117,7 @@ function render() {
                 let tile_y = parseInt(this.attributes.y.value);
                 if (is_valid_placement(tile_x, tile_y)) {
                     place_tile(tile_x, tile_y, current_tile);
+                    score_tile(tile_x, tile_y);
                     recalculate_subgrid(tile_x, tile_y);
                     current_tile = tiles.pop();
                     render();
@@ -197,6 +198,100 @@ function is_valid_placement(x, y) {
 function place_tile(x, y, tile) {
     grid[y][x].type = "tile";
     grid[y][x].tile = tile;
+}
+
+function score_tile(x, y) {
+    let left_color = grid[y][x].tile[0];
+    let top_color = grid[y][x].tile[1];
+    let right_color = grid[y][x].tile[2];
+    let bottom_color = grid[y][x].tile[3];
+    let scores = [{
+        color: left_color,
+        score: score_color(left_color, x-1, y, 2),
+    }, {
+        color: top_color,
+        score: score_color(top_color, x, y-1, 3),
+    }, {
+        color: right_color,
+        score: score_color(right_color, x+1, y, 0),
+    }, {
+        color: bottom_color,
+        score: score_color(bottom_color, x, y+1, 1),
+    }];
+    let i = 0;
+    while (i < scores.length) {
+        if (scores[i].color == scores[(i+1)%scores.length].color) {
+            if (scores[i].score == null || scores[ (i+1)%scores.length].score == null) {
+                scores[i].score = null;
+                scores[(i+1)%scores.length].score = null;
+                i++
+            } else {
+                scores[i].score += scores[ (i+1)%scores.length].score;
+                scores.splice((i+1)%scores.length, 1);
+            }
+        } else {
+            i++;
+        }
+    }
+    let final_score = 0;
+    for (let i = 0; i < scores.length; i++) {
+        if (scores[i].score != null) {
+            if (scores[i].score < 4) {
+                final_score += (scores[i].score + 1);
+            } else {
+                final_score += (scores[i].score + 1) * 2;
+            }
+        }
+    }
+    if (final_score != 0) {
+        score += final_score;
+    }
+}
+
+function score_color(color, x, y, origin) {
+    let tile = grid[y][x];
+    if (tile.type == "space") {
+        return null;
+    }
+    let tally = 1;
+    let next_tiles = [];
+    if (tile[(origin+1)%4] == color) {
+        next_tiles.push((origin+1)%4);
+    }
+    if (tile[(origin+3)%4] == color) {
+        next_tiles.push((origin+3)%4);
+    }
+    if ((tile[(origin+1)%4] == color || tile[(origin+3)%4] == color) && tile[(origin+2)%4] == color) {
+        next_tiles.push((origin+2)%4);
+    }
+    for (let i = 0; i < next_tiles.length; i++) {
+        if (next_tile[i] == 0) {
+            let s = score_color(color, x-1, y, 2);
+            if (s == null) {
+                return null;
+            }
+            tally += s;
+        } else if (next_tile[i] == 1) {
+            let s = score_color(color, x, y-1, 3);
+            if (s == null) {
+                return null;
+            }
+            tally += s;
+        } else if (next_tile[i] == 2) {
+            let s = score_color(color, x+1, y, 0);
+            if (s == null) {
+                return null;
+            }
+            tally += s;
+        } else {
+            let s = score_color(color, x, y+1, 1);
+            if (s == null) {
+                return null;
+            }
+            tally += s;
+        }
+    }
+    return tally;
 }
 
 function recalculate_subgrid(x, y) {
